2be70a685c918232b64085b4d4ae9e6955955852
jakapongjoke <jakapong991@hotmail.com>
2023-02-08 18:26:24 +0700

    Update driveAPI.tsx

--- src/_config/driveAPI.tsx
+++ src/_config/driveAPI.tsx
@@ -2,7 +2,7 @@ export default {
     SCOPE: 'https://www.googleapis.com/auth/drive',
     RESPONSE_TYPE: 'code',
     ACCESS_TYPE: 'offline',
-    REDIRECT_URI: 'https://warroom-miniapp-note-frontend.pages.dev/auth',
+    REDIRECT_URI: 'https://staging.warroom.wisesight.com/integration/miniapp/Note-dev%235345',
     CLIENT_ID: '628499684372-mapu5klhk62lp20dusv9a6vk71q4rv1p.apps.googleusercontent.com',
     CLIENT_SECRET: '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',
     GRANT_TYPE: 'authorization_code',
