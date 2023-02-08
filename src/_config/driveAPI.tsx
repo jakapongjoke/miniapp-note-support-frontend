bd3d57c5907ebe4926d696e7ccf2f7bdea80cb75
rittidetnpk1 <rittidetnpk1@gmail.com>
2023-02-08 21:18:53 +0700

    [EDIT] ตรวจสอบ login gmail

--- src/_config/driveAPI.tsx
+++ src/_config/driveAPI.tsx
@@ -1,8 +1,8 @@
 export default {
     SCOPE: 'https://www.googleapis.com/auth/drive',
     RESPONSE_TYPE: 'code',
-    ACCESS_TYPE: 'online',
-    REDIRECT_URI: 'https://staging.warroom.wisesight.com/integration/miniapp/Note-dev%235345',
+    ACCESS_TYPE: 'offline',
+    REDIRECT_URI: 'https://warroom-miniapp-note-frontend.pages.dev/auth',
     CLIENT_ID: '628499684372-mapu5klhk62lp20dusv9a6vk71q4rv1p.apps.googleusercontent.com',
     CLIENT_SECRET: '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',
     GRANT_TYPE: 'authorization_code',
